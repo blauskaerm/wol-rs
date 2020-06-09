bac2c7f204456a3788d6eafe1860ba68da0005e7
BlauskaerM <blauskaerm@protonmail.ch>
2020-06-09 06:48:47 +0000

    Able to send UDP message "HELLO" to localhost 8080

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::net::UdpSocket;
+
+include!("wol.rs");
+
 fn main() {
-    println!("Hello, world!");
+
+    let sync_stream = [0xFF;6];
+    let dst_mac = [
+        0x1c,
+        0x39,
+        0x47,
+        0xd0,
+        0x9d,
+        0xec
+    ];
+
+
+    let buffer = [0x48,0x45,0x4C,0x4C,0x4F,0x0A];
+
+    let socket = UdpSocket::bind("127.0.0.1:34254")
+        .expect("couldn't bind to address");
+
+    socket.send_to(&buffer, "127.0.0.1:8080")
+        .expect("couldn't send data");
+
+
+    println!("{}", sync_stream[0]);
 }
